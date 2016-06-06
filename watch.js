0ca1adbed7aef4b20e876c575e6028bf948f0207
walton seymour <bigw369@gmail.com>
2016-06-06 17:36:48 -0400

    begins to tie watch and graph together

--- watch.js
+++ watch.js
@@ -1,18 +1,31 @@
 const fs = require('fs');
 const readline = require('readline');
+const graph = require('./graph');
+graph.graph();
 
+const offset = 0;
+var lastSeen = null;
+var currentRate = 0;
 
+// Watches log file for changes every 100ms
 fs.watchFile("./test.log", {interval: 100}, (curr, prev) => {
   var lineReader = readline.createInterface({
-    input: fs.createReadStream('./test.log')
+    input: fs.createReadStream('./test.log', {start: offset})
   });
 
   lineReader.on('line', (line) => {
-    line = parseLine(line);
-    console.log(line);
+    // + 1 for newline character not included in string
+    offset += line.length + 1;
+
+    const parsed = parseLine(line);
+
+    // send off to be processed and graphed.
+    if (parsed !== null){
+      lastSeen = parsed.date;
+      graph.addData(line.user, parseInt(line.size));
+   }
   });
 });
-
 
 function parseLine(line){
   const pattern = /^([\d.]+) ([\w.-]+) ([\w.-]+) \[([\w/: -]+)\] "([\w /.]+)" ([\d]{3}) ([\d]+)$/;
@@ -29,5 +42,4 @@ function parseLine(line){
     status: match[6],
     size: match[7]
   };
-
 }
