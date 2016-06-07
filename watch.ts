78124eeda83fbf02b29f00f4eb173698a994b599
walton seymour <bigw369@gmail.com>
2016-06-07 12:58:33 -0400

    removes watch.js

--- watch.ts
+++ watch.ts
@@ -2,35 +2,26 @@
 
 import * as fs from 'fs';
 import * as readline from 'readline';
-// import * as graph from './graph';
-//
-// graph.graph();
 
 let offset : number = 0;
 let lastSeen : string = null;
 let requestsPerMin: number = 0;
 
 // watches log file for changes every 100ms
-fs.watchFile("./test.log", {interval: 100}, (curr, prev) => {
-  interface options {
-    start: number
-  }
+fs.watchFile("./test.log", {interval: 100}, () => {
   let lineReader = readline.createInterface({
     // opens read stream starting at the last point we've seen
-    input: fs.createReadStream('./test.log', <options>{start: offset})
+    input: fs.createReadStream('./test.log', <any>{start: offset})
   });
 
   lineReader.on('line', (line) => {
     // + 1 for newline character not included in string
     offset += line.length + 1;
-
     let parsed = parseLine(line);
 
     // send off to be processed and graphed.
     if (parsed !== null){
       lastSeen = parsed.date;
-      //graph.addData(parsed.date, parseInt(parsed.size));
-      console.log(parsed.status);
    }
   });
 });
