dd47bf4095f84156e7c4f06a404b83e7be9f00c9
walton seymour <bigw369@gmail.com>
2016-06-14 02:16:33 -0400

    adds testing framework

---
+++ test/alerts.ts
@@ -0,0 +1,9 @@
+/// <reference path='../typings/index.d.ts' />
+/// <reference path='../node.d.ts' />
+import { assert } from 'chai';
+
+describe('Alerts', () => {
+  it('shoud something', () =>{
+    assert(1 === 1);
+  });
+});
