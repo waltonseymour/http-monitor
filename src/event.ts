0f5dfca583699d9bf8224f69b8f6d94657db6afd
walton seymour <bigw369@gmail.com>
2016-06-14 14:34:52 -0400

    cleans up Event constructor

--- src/event.ts
+++ src/event.ts
@@ -14,25 +14,20 @@ export class Event {
   constructor(line: string){
     const pattern = /^([\d.-]+) ([\w.-]+) ([\w.-]+) \[([\w/: -]+)\] "([\w /.-]+)" ([\d]{3}|-) ([\d-]+)$/;
     const match = pattern.exec(line);
-    if (match === null) {
-      // might want to throw exception
-      return null;
-    }
-    else {
-      // regex to extract section ie. /posts /users ..etc
-      const sectionPattern = / (\/\w+)\//;
-      let sectionMatch = sectionPattern.exec(match[5]);
-      this.ip = match[1];
-      this.rfc = match[2];
-      this.user = match[3];
-      this.date = moment(match[4], 'MM/DD/YYYY:HH:mm:ss Z').toDate();
-      this.request = match[5];
-      this.section = sectionMatch !== null ? sectionMatch[1] : '/';
-      this.status = parseInt(match[6]);
-      this.size = parseInt(match[7]);
-    }
-  }
 
+    if (match === null) { throw new Error('Invalid log format'); }
+
+    const sectionPattern = / (\/\w+)\//;
+    let sectionMatch = sectionPattern.exec(match[5]);
+    this.ip = match[1];
+    this.rfc = match[2];
+    this.user = match[3];
+    this.date = moment(match[4], 'MM/DD/YYYY:HH:mm:ss Z').toDate();
+    this.request = match[5];
+    this.section = sectionMatch !== null ? sectionMatch[1] : '/';
+    this.status = parseInt(match[6]);
+    this.size = parseInt(match[7]);
+  }
 };
 
 export class EventProcessor {
